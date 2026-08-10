78be900fa8a2e6c46fb0e267e3ffdaf56475e43c
manan-neural <mananvashi123@gmail.com>
2026-08-10 20:41:45 +0530

    Time: 31 ms (5.64%), Space: 111.1 MB (5.09%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        boolean check = false;
+        HashSet <Integer> set =  new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                check = true;
+            }
+            set.add(nums[i]);
+        }
+        return check;
+    }
+}
