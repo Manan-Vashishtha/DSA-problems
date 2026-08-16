2c31a16f1ff2320254322f34d9b8523eb8482d17
manan-neural <mananvashi123@gmail.com>
2026-08-16 20:01:58 +0530

    Time: 22 ms (6.48%), Space: 47 MB (5.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character, Integer> l= new HashMap<>();
-        HashMap<Character, Integer> l1= new HashMap<>();
+        int count =0;
+        if(t.length() != s.length()){
+            return false;
+        }
         for (int i = 0; i < s.length(); i++) {
             l.put(s.charAt(i), l.getOrDefault(s.charAt(i), 0)+1);
         }
         for (int i = 0; i < t.length(); i++) {
-            l1.put(t.charAt(i), l1.getOrDefault(t.charAt(i), 0)+1);
+            if(l.containsKey(t.charAt(i)) && l.getOrDefault(t.charAt(i), -1)>0){
+                l.put(t.charAt(i), l.getOrDefault(t.charAt(i), 0)-1);
+                if(l.get(t.charAt(i))==0){
+                    l.remove(t.charAt(i));
+                }
+                count++;
+            }
         }
-
-        if (l.equals(l1)) {
-            return true;
-        }
-         return false;   
-    
         
+        if(count == t.length()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
