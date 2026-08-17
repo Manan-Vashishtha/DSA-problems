b0ba737a364bcd5d7f92de84e03b5d8260bd1c7b
manan-neural <mananvashi123@gmail.com>
2026-08-17 21:45:40 +0530

    Update 0242-valid-anagram.java

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,10 +6,10 @@ class Solution {
             return false;
         }
         for (int i = 0; i < s.length(); i++) {
-            l.put(s.charAt(i), l.getOrDefault(s.charAt(i), 0)+1);
+            l.put(s.charAt( i), l.getOrDefault(s.charAt(i), 0)+1);
         }
         for (int i = 0; i < t.length(); i++) {
-            if(l.containsKey(t.charAt(i)) && l.getOrDefault(t.charAt(i), -1)>0){
+            if( l.containsKey(t.charAt(i)) && l.getOrDefault(t.charAt(i), -1)>0){
                 l.put(t.charAt(i), l.getOrDefault(t.charAt(i), 0)-1);
                 if(l.get(t.charAt(i))==0){
                     l.remove(t.charAt(i));
