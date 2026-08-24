9c6095a2aae5fc5889ad88e952d4c27431e41751
manan-neural <mananvashi123@gmail.com>
2026-08-24 20:20:43 +0530

    Time: 5 ms (83.14%), Space: 76.9 MB (96.03%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,26 +1,18 @@
 class Solution {
-    static{
-        for(int i = 0; i<2; i++){
-             maxArea(new int[]{0,0});
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int maxA = 0;
+
+        while(left<right){
+            int currA =  Math.min(height[left], height[right]) * (right-left);
+            maxA = Math.max(maxA, currA);
+            if(height[left] > height[right]){
+                right--;
+            }else{
+                left++;
+            }
         }
-    }
-    public static int maxArea(int[] h) {
-        int srt  = 0;
-        int end = h.length-1;
-        int maxWater  = 0;
-        while(srt < end){
-            int minH = Math.min(h[srt], h[end]);
-            int area = minH * (end-srt);
-            maxWater = Math.max(maxWater, area);
-           while(srt < end && h[srt] <= minH){
-            srt++;
-           }
-           while(srt < end && h[end] <= minH){
-            end--;
-           }
-            
-        }
-        return maxWater;
-        
+        return maxA;
     }
 }
