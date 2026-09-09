5e25583478ad3e5e65ab6dc94832922186c1863b
manan-neural <mananvashi123@gmail.com>
2026-09-09 18:37:44 +0530

    Time: 3 ms (17.91%), Space: 47.7 MB (57.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k = 0;
-        for(int i=0; i<nums.length; i++){
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
             if(nums[i] != 0){
-                int temp = nums[k];
-                nums[k] = nums[i];
-                nums[i] = temp;
-                k++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
         }
     }
