8aa428b544d2a9c66bfdd6c8667b9236e65c7c7e
manan-neural <mananvashi123@gmail.com>
2026-09-20 23:34:52 +0530

    Time: 1 ms (100%), Space: 44 MB (50.47%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,11 +1,9 @@
 class Solution {
     public int reverseDegree(String s) {
-        int j = 1;
         int sum = 0;
         for (int i = 0; i <s.length(); i++) {
             int ch = 123- s.charAt(i);
-            sum += ch*j;
-            j++;
+            sum += ch*(i+1);
         }
         return sum;
     }
